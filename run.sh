02006f0cc5732bd7dff2307875af0052e0e1064a
Neil Ellis <hello@neilellis.me>
2015-06-16 22:46:54 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 echo "license_key=$NEW_RELIC_LICENSE_KEY" > /etc/nrsysmond.cfg
 echo "labels=${NEW_RELIC_LABELS:-host:${TUTUM_NODE_HOSTNAME:-unknown}}" >> /etc/nrsysmond.cfg
-echo "hostname=${NEW_RELIC_HOSTNAME:-${TUTUM_NODE_HOSTNAME}}">> /etc/nrsysmond.cfg
+echo "hostname=${NEW_RELIC_HOSTNAME:-${TUTUM_NODE_HOSTNAME}}" >> /etc/nrsysmond.cfg
+echo "docker=/var/run/docker.sock" >> /etc/nrsysmond.cfg
 cat nrsysmond.cfg >> /etc/nrsysmond.cfg
 /usr/sbin/nrsysmond -c /etc/nrsysmond.cfg  -l /dev/stdout -f
