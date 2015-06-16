aa051a89dfbdfe8f427100eb8a57259855118c6b
Neil Ellis <hello@neilellis.me>
2015-06-16 21:48:56 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "license_key=$NEW_RELIC_LICENSE_KEY" > /etc/nrsysmond.cfg
 echo "labels=$NEW_RELIC_LABELS" >> /etc/nrsysmond.cfg
-echo "hostname=${NEW_RELIC_HOST:-${TUTUM_SERVICE_HOSTNAME}}"
+echo "hostname=${NEW_RELIC_HOST:-${TUTUM_NODE_HOSTNAME}}"
 cat nrsysmond.cfg >> /etc/nrsysmond.cfg
 /usr/sbin/nrsysmond -c /etc/nrsysmond.cfg  -l /dev/stdout -f
