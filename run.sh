1c5fc92e0ab52b8ea48be0daf54bd1b41542cfd0
neilellis <hello@neilellis.me>
2015-06-16 21:00:12 +0100

    Initial work

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 echo "license_key=$NEW_RELIC_LICENSE_KEY" > /etc/nrsysmond.cfg
+echo "labels=$NEW_RELIC_LABELS" >> /etc/nrsysmond.cfg
 cat nrsysmond.cfg >> /etc/nrsysmond.cfg
 /usr/sbin/nrsysmond -c /etc/nrsysmond.cfg  -l /dev/stdout -f
